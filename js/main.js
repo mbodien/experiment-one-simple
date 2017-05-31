b2bb90dd7dbb4a3288bb95285dc89636b0097f8a
Matt <mbodien@gmail.com>
2017-05-31 18:27:57 +0100

    May 30

--- js/main.js
+++ js/main.js
@@ -13,37 +13,100 @@ $(".errorMessage").hide();
 $(".facebookSharePicture").hide();
 
 
-var data = []; // all data will be stored here
+
 
 function nextPage() {
-    var pageHide = "#page" + currentPage;
+    var pageHide = "#page" + currentPage; // #page2
     currentPage ++;
-    var pageShow = "#page" + currentPage;
+    var pageShow = "#page" + currentPage; // #page3
     $(pageHide).hide();
     $(pageShow).show();
 }
 
-/* Demo data */
+/* Data saving */
+
+var data = []; // all data will be stored here
+
+var d = new Date();
+
+function expStartDataSave() {
+    var expStartTime = d.getTime();
+    var startTime = "experimentStart: " +expStartTime;
+    data.push(startTime);
+}
 
 function demoDataSave() {
-    var age = $("input[name=age]:checked").val();
-    var gender = $("input[name=gender]:checked").val();
-    var facebookUsage = $("input[name=facebookUsage]:checked").val();
-    data.push(age, gender, facebookUsage);
+    if (!$("input[name=gender]:checked").val() || !$("input[name=facebookUsage]:checked").val()) {
+        $("#demosDataError").show();
+    } else {
+        var age = $("input[name=age]:checked").val();
+        var ageSave = "age: " + age;
+        var gender = $("input[name=gender]:checked").val();
+        var genderSave = "gender: " + gender;
+        var facebookUsage = $("input[name=facebookUsage]:checked").val();
+        var facebookUsageSave = "facebookUsage: " + facebookUsage;
+        data.push(ageSave, genderSave, facebookUsageSave);
+        nextPage();
+    }
 }
 
 function politicalDataSave() {
-    var partyID = $("input[name=partyID]:checked").val();
-    var ideology = $("input[name=ideology]:checked").val();
-    var ideologyImportance = $("input[name=partyImportance]:checked").val();
     if (!$("input[name=partyID]:checked").val() || !$("input[name=ideology]:checked").val() || !$("input[name=partyImportance]:checked").val()) {
         $("#politicalDataError").show();
     } else {
-        data.push(partyID, ideology,ideologyImportance);
+        var partyID = $("input[name=partyID]:checked").val();
+        var partyIDsave = "party: " + partyID;
+        var ideology = $("input[name=ideology]:checked").val();
+        var ideologySave = "ideology: " + ideology;
+        var socialValues = $("input[name=socialValues]:checked").val();
+        var socialValuesSave = "socialValues: " + socialValues;
+        var ideologyImportance = $("input[name=partyImportance]:checked").val();
+        var ideologyImportanceSave = "importance: " + ideologyImportance;
+        data.push(partyIDsave, ideologySave, socialValuesSave, ideologyImportanceSave);
         decideConditions();
         setExperimentDirections();
         nextPage();
     }
+}
+
+function trial1StartTime() {
+    var startTime = d.getTime();
+    var startTimeSave = "trial1 start: " + startTime;
+    data.push(startTimeSave);
+}
+
+function trial2StartTime() {
+    var startTime = d.getTime();
+    var startTimeSave = "trial2 start: " + startTime;
+    data.push(startTimeSave);
+}
+
+function saveMemeData() {
+    var memeNumber = "memeID:" + memeID;
+    var memeScore = $("input[name=likertMeme]:checked").val();
+    var likertData = "trialID_" + trialID + "_" + memeNumber + ": " + memeScore;
+    var buttonClickedTime = d.getTime();
+    var RTData = "trialID_" + trialID + "_" + memeNumber + ": " + buttonClickedTime;
+    data.push(likertData, RTData);
+    console.log(RTData);
+    console.log(likertData);
+}
+
+function saveFacebookLikeData() {
+    memeLike = $("input[name=likertMemeFB]:checked").val();
+    data.push("facebookLike: " + memeLike);
+}
+
+function saveFacebookShareData() {
+    memeShare = $("input[name=likertMemeFB]:checked").val();
+    data.push("facebookShare: " + memeShare);
+}
+
+
+// database write
+
+function writeData() {
+    firebase.database().ref('/').push(data)
 }
 
 
@@ -135,6 +198,7 @@ function decideTargetSource() {
 }
 
 
+
 /* determine target meme properites */
 
 var targetMemeCopy = "";
@@ -166,7 +230,7 @@ function determineFillerMemeProps(memeID) {
 
 function setExperimentDirections() {
     if (revealOrderCondition === "messageFirstCondition") {
-        $("#trial1Directions").text("There are 4 different memes that you will see.\n\nBut, Before we show you the whole memes, we want to know how you feel about just the message of each meme.\n\nPlease record how much you resonate with the person.\n\nBy resonate we mean how much does the message speak to you.");
+        $("#trial1Directions").text("In a moment, we will show you 4 different memes that we will ask you to evaluate.\n\nBut, before we show you the whole memes, we want to know how you feel about just the message of each meme.\n\nPlease record how much you resonate with the message.\n\nBy resonate we mean how much does the message speak to you in a positive, inspiring, and meaningful way.");
     } else if (revealOrderCondition === "sourceFirstCondition") {
         $("#trial1Directions").text("There are 4 different memes that you will see.\n\nBut, before we show you the whole memes, we want to know how you feel about just person who authored each message.\n\nPlease record how much you resonate with the person that you see.\n\nBy resonate we mean how much do you find the person positive, inspiring, and meaningful.");
     }
@@ -269,19 +333,6 @@ function checkWhichTrial() {
         }
 }
 
-
-function saveMemeData() {
-    var memeNumber = "memeID = " + memeID;
-    var memeScore = $("input[name=likertMeme]:checked").val();
-    var likertData = "trialID = " + trialID + " " + memeNumber + ": " + memeScore;
-    var d = new Date();
-    var buttonClickedTime = d.getTime();
-    var RTData = "trialID = " + trialID + " " + memeNumber + ": " + buttonClickedTime;
-    data.push(likertData, RTData);
-    console.log(RTData);
-    console.log(likertData);
-}
-
 function clearMemeLikert() {
     $("input[name=likertMeme]").prop("checked",false);
 }
@@ -292,6 +343,7 @@ function checkLikertError() {   // make sure selection made, otherwise throw err
         $("#memeLikertError").show();
     }
     else {
+        $("#memeLikertError").hide();
         saveMemeData();  // creates variable to save data
         clearMemeLikert(); // unchecks the likert radio buttons
         memeCounter(); // +1 meme ID counter, and checks trial counter
@@ -311,7 +363,7 @@ function facebookEvaluation() {
         saveFacebookLikeData();
         facebookEvalPage ++;
         $("input[name=likertMemeFB]").prop("checked",false); // reset likert question
-        $("#facebookDirections").text("Which of the memes that you have seen would you be most likely to 'like' on Facebook?\nSelect only 1.");
+        $("#facebookDirections").text("Which of the memes that you have seen would you be most likely to 'share' on Facebook?\nSelect only 1.");
         $("#facebookEvalPicture-0").attr("src","http://audioenhancement.com/wp-content/uploads/2014/05/Share-on-Facebook-LG.png");
         $("#facebookEvalPicture-1").attr("src","http://audioenhancement.com/wp-content/uploads/2014/05/Share-on-Facebook-LG.png");
         $("#facebookEvalPicture-2").attr("src","http://audioenhancement.com/wp-content/uploads/2014/05/Share-on-Facebook-LG.png");
@@ -319,66 +371,45 @@ function facebookEvaluation() {
     } else {  // if completed "share" question
         saveFacebookShareData();
         nextPage();
+        writeData();
     }
 }
 
-function saveFacebookLikeData() {
-    memeLike = $("input[name=likertMemeFB]:checked").val();
-    data.push("facebook Like : " + memeLike);
-}
-
-function saveFacebookShareData() {
-    memeShare = $("input[name=likertMemeFB]:checked").val();
-    data.push("facebook Share : " + memeShare);
-}
-
-
 
 var select = "";
-for (i=18;i<=100;i++) {
+for (i=17;i<=100;i++) {
     select += "<option val=" + i + ">" + i + "</option>";
 }
 $("#18_100").html(select);
 
 
 
-// database write
-
-function writeData(userID, age, gender) {
-    firebase.database().ref('/').push({
-        "userID": userID,
-        "age": age,
-        "gender": gender
-    })
-}
-
-
 /* objects of all meme content */
 
 var targetMemeCopyLibrary = {
     equalityCondition: {
-        "BO": "Everyone starts with the capacity to achieve great success. There should be an equal opportunity to everyone to forge that achievement.",
+        "BO": "Everyone is born with equal capacity for achieving success. Citizens in this country deserve an equal opportunity to reach that potential.",
         "HC": "Everyone is born with equal capacity for achieving success. Citizens in this country deserve an equal opportunity to reach that potential.",
-        "SP": "Common sense tells us that every person is born with great potential. Common sense also tells us that we need to make sure everyone has equal opportunity to make it happen.",
+        "SP": "Everyone is born the same. Everyone could be great. But, there isn’t an equal opportunity in this country to make it happen. There should be.",
         "DT": "Everyone is born the same. Everyone could be great. But, there isn’t an equal opportunity in this country to make it happen. There should be."
     },
     selfRelianceCondition: {
-        "BO": "Understand that your success in life won’t be determined just by what’s given to you, but by how hard you try. Self perseverance and self reliance should be values that we fight to uphold.",
-        "HC": "Strength comes from learning how to handle problems on your own. It’s a crucial skill that everyone must learn. We should always do what we can to encourage that learning.",
-        "SP": "Let people figure things out on their own. Learn how to take care of yourself and you’ve learned the most important thing you can.",
-        "DT": "Let people take care of themselves. It makes them strong. And it makes them proud."
+        "BO": "We must let individuals take care of themselves, and be cautious of over-involving ourselves in people’s lives. Strength comes from learning how to solve one’s own problems.",
+        "HC": "We must let individuals take care of themselves, and be cautious of over-involving ourselves in people’s lives. Strength comes from learning how to solve one’s own problems.",
+        "SP": "Let people take care of themselves. We can’t do everything for them. If we let people figure things out on their own, it will make them strong. And make them proud.",
+        "DT": "Let people take care of themselves. We can’t do everything for them. If we let people figure things out on their own, it will make them strong. And make them proud."
     },
     toleranceCondition: {
-        "BO": "People should be proud of their identity, and they should feel free and able to share that identity. It is our job to make sure that that freedom persists.",
-        "HC": "We have the opportunity to embrace not only our own families and communities, but those families and communities that have so much more to offer. Why would we pass on that?",
-        "SP": "Sometimes we all feel a bit like the outsider. Everyone can relate to what it’s like to feel excluded. We should always do our best to include others, even if they don’t make choices that we always agree with.",
-        "DT": "I get along with people of every type. Not just because good people are everywhere but because I’ve learned that there’s more to be gained by accepting and listening to them than by shutting them out."
+        "BO": "I’ve always been someone who gets along with others. By listening, by embracing others’ unique point of view, you give yourself the chance to connect with those you’d never otherwise associate.",
+        "HC": "I’ve always been someone who gets along with others. By listening, by embracing others’ unique point of view, you give yourself the chance to connect with those you’d never otherwise associate.",
+        "SP": "I get along with people of every type. Not just because good people are everywhere but because I’ve learned that there’s more to be gained by accepting and listening than by shutting them out.",
+        "DT": "I get along with people of every type. Not just because good people are everywhere but because I’ve learned that there’s more to be gained by accepting and listening than by shutting them out."
     },
     traditionalismCondition: {
-        "BO": "Those traditions and values that we have inherited from our parents, and their parents before them, are integral to who we are. We should defend these ideas like we would defend our current lives.",
-        "HC": "When our families are strong, we are strong. The traditional values that come from good families are the bedrock of our country. We should never forget that.",
-        "SP": "You bet family is important. I’d say the values that come from good, loving families is the most important feature of society today. We should treasure those family values.",
-        "DT": "We shouldn’t be afraid of sticking to the things that have worked for us for so long. We should never lose them. We should never forget them. This should be priority number one."
+        "BO": "Families play a foundational role in society. It’s the responsibility of every parent to ensure that the next generation learns the long-standing values that define who we are, and who we strive to be.",
+        "HC": "Families play a foundational role in society. It’s the responsibility of every parent to ensure that the next generation learns the long-standing values that define who we are, and who we strive to be.",
+        "SP": "Families are the bedrock of society. Healthy families make sure that those long-established values that make us who we are make it to the next generation, and every generation after.",
+        "DT": "Families are the bedrock of society. Healthy families make sure that those long-established values that make us who we are make it to the next generation, and every generation after."
     }
 };
 
